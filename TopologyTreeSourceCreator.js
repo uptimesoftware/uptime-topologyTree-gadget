71a2e9929f3745a0e4c06b5ae4a2b9a68275ddf2
jchen <jchen@dev-jchen.uptimesoftware.com>
2013-05-08 16:16:09 -0400

    Used autoformatting

--- TopologyTreeSourceCreator.js
+++ TopologyTreeSourceCreator.js
@@ -1,28 +1,27 @@
-TopologyTreeSourceCreator = function(){
+TopologyTreeSourceCreator = function() {
 	var uptime_api = new uptimeApi();
 	var availableElementsForTree = {};
 	var treeRenderingFunction;
 
-	this.getSource = function(successCallback,errorCallback){
+	this.getSource = function(successCallback, errorCallback) {
 
 		treeRenderingFunction = successCallback;
-		uptime_api.getElements("", pushIntoElementArray,errorCallback);
+		uptime_api.getElements("", pushIntoElementArray, errorCallback);
 	}
 
-	var pushIntoElementArray = function(elements){
-		numElements = elements.length-1;
+	var pushIntoElementArray = function(elements) {
+		numElements = elements.length - 1;
 		var allStatusTasks = [];
 
-		$.each(elements, function(index,element) {	
-			if (element.isMonitored)
-			{
+		$.each(elements, function(index, element) {
+			if (element.isMonitored) {
 				var statusTask = new $.Deferred();
 				allStatusTasks.push(statusTask);
 				var elementNode = new Object();
 				elementNode.id = element.id;
 				elementNode.name = element.name;
 				elementNode.typeSubtypeName = element.typeSubtypeName;
-				setupStatus(element.id, function(statusInfo){
+				setupStatus(element.id, function(statusInfo) {
 					elementNode.status = statusInfo.status;
 					elementNode.parents = statusInfo.parentsStatus;
 					elementNode.monitorStatus = statusInfo.monitorStatuses;
@@ -35,7 +34,7 @@ TopologyTreeSourceCreator = function(){
 		$.when.apply($, allStatusTasks).done(buildTreeInMemory);
 	}
 
-	var setupStatus = function(id, handleElementStatus){
+	var setupStatus = function(id, handleElementStatus) {
 		uptime_api.getElementStatus(id, function(elementStatus) {
 			var statusInfo = {};
 			statusInfo.message = elementStatus.message;
@@ -46,23 +45,24 @@ TopologyTreeSourceCreator = function(){
 		});
 	}
 
-	var getAdditionalStatus = function(additionalStatus){
-		// At least one Topological Parent, push parents into the element's 'parents' array
+	var getAdditionalStatus = function(additionalStatus) {
+		// At least one Topological Parent, push parents into the element's
+		// 'parents' array
 		var additionalStatusArray = new Array();
-		if(additionalStatus.length != 0) {
+		if (additionalStatus.length != 0) {
 			$.each(additionalStatus, function(j, additionalElementStatus) {
-				additionalStatusArray.push( {
-					id: additionalElementStatus.id,
-					name: additionalElementStatus.name,
-					status: additionalElementStatus.status,
-					
+				additionalStatusArray.push({
+					id : additionalElementStatus.id,
+					name : additionalElementStatus.name,
+					status : additionalElementStatus.status,
+
 				});
 			});
 		}
 		return additionalStatusArray;
 	}
-	
-	var createRoot = function(){
+
+	var createRoot = function() {
 		var root = new Object();
 		root.entityId = 0;
 		root.entityName = "My Infrastructure";
@@ -77,62 +77,54 @@ TopologyTreeSourceCreator = function(){
 	var buildTreeInMemory = function() {
 		var elementsOnTree = {};
 		var root = createRoot();
-		$.each(availableElementsForTree, function(i, currentNode){
-			if (hasNoParents(currentNode))
-			{
+		$.each(availableElementsForTree, function(i, currentNode) {
+			if (hasNoParents(currentNode)) {
 				var childNode = getNodeOnTree(elementsOnTree, currentNode);
 				elementsOnTree[childNode.entityId] = childNode;
 				root.dependents.push(childNode);
 			}
-			if (currentNode.status != "OK")
-			{
+			if (currentNode.status != "OK") {
 				createBranch(availableElementsForTree, elementsOnTree, currentNode, root);
 			}
 		});
 		treeRenderingFunction(decompressTree(root));
 	}
 
-	var decompressTree =  function(source){
+	var decompressTree = function(source) {
 		return jQuery.extend(true, {}, source);
 	}
 
-
-	var createBranch = function (availableElementsForTree, elementsOnTree, node, root){
+	var createBranch = function(availableElementsForTree, elementsOnTree, node, root) {
 		var childNode = getNodeOnTree(elementsOnTree, node);
 		elementsOnTree[childNode.entityId] = childNode;
 
-
-			$.each(node.parents, function(i, parent){
-				var parentOnTree = elementsOnTree[parent.id];
-				if (typeof parentOnTree=="undefined")
-				{
-					var parentNode = availableElementsForTree[parent.id];
-					if (isParentNodeAvailableForTree(parentNode))
-					{
-						parentOnTree = createBranch(availableElementsForTree, elementsOnTree, parentNode, root);
-					}else{
-						parentOnTree = buildInvisibleParentNode(parent, elementsOnTree, root);
-
-					}
+		$.each(node.parents, function(i, parent) {
+			var parentOnTree = elementsOnTree[parent.id];
+			if (typeof parentOnTree == "undefined") {
+				var parentNode = availableElementsForTree[parent.id];
+				if (isParentNodeAvailableForTree(parentNode)) {
+					parentOnTree = createBranch(availableElementsForTree, elementsOnTree, parentNode, root);
+				} else {
+					parentOnTree = buildInvisibleParentNode(parent, elementsOnTree, root);
 
 				}
 
-				if (isParentExist(parentOnTree) && !isAlreadyDependent(parentOnTree, childNode))
-				{
-					parentOnTree.dependents.push(childNode);
-				}
-			});
-		
-		
+			}
+
+			if (isParentExist(parentOnTree) && !isAlreadyDependent(parentOnTree, childNode)) {
+				parentOnTree.dependents.push(childNode);
+			}
+		});
+
 		return childNode;
 	}
 
-	var buildInvisibleParentNode = function(node, elementsOnTree, root){
-		if(typeof elementsOnTree[node.id]!="undefined"){
+	var buildInvisibleParentNode = function(node, elementsOnTree, root) {
+		if (typeof elementsOnTree[node.id] != "undefined") {
 			var parentOnTree = elementsOnTree[node.id];
 			elementsOnTree[parentOnTree.entityId] = parentOnTree;
-			root.dependents.push(parentOnTree);		
-			return parentOnTree;	
+			root.dependents.push(parentOnTree);
+			return parentOnTree;
 		}
 		var newNode = new Object();
 		newNode.entityId = node.id;
@@ -143,32 +135,34 @@ TopologyTreeSourceCreator = function(){
 		newNode.type = "Invisible";
 		newNode.dependents = new Array();
 		elementsOnTree[newNode.entityId] = newNode;
-		root.dependents.push(newNode);		
+		root.dependents.push(newNode);
 		return newNode;
 	}
 
-	var isParentNodeAvailableForTree = function(parentNode){
+	var isParentNodeAvailableForTree = function(parentNode) {
 		return parentNode != null;
 	}
 
-	var isParentExist = function(parent){
-		return typeof parent!="undefined";
+	var isParentExist = function(parent) {
+		return typeof parent != "undefined";
 	}
 
-	var isAlreadyDependent = function(parent, child){
-		var matchedElements = $.grep(parent.dependents, function(e){ return e.entityId == child.entityId; });
-		return matchedElements!=0;
+	var isAlreadyDependent = function(parent, child) {
+		var matchedElements = $.grep(parent.dependents, function(e) {
+			return e.entityId == child.entityId;
+		});
+		return matchedElements != 0;
 	}
 
-	var getNodeOnTree = function(elementsOnTree, currentNode){
+	var getNodeOnTree = function(elementsOnTree, currentNode) {
 
-		if(typeof elementsOnTree[currentNode.id]!="undefined"){
-			return elementsOnTree[currentNode.id];	
+		if (typeof elementsOnTree[currentNode.id] != "undefined") {
+			return elementsOnTree[currentNode.id];
 		}
 		return createNodeOnTree(currentNode);
 	}
 
-	var createNodeOnTree = function(currentNode){
+	var createNodeOnTree = function(currentNode) {
 		var newNode = new Object();
 		newNode.entityId = currentNode.id;
 		newNode.entityName = currentNode.name;
@@ -180,8 +174,8 @@ TopologyTreeSourceCreator = function(){
 		return newNode;
 	}
 
-	var hasNoParents = function(node){
+	var hasNoParents = function(node) {
 		return node.parents.length == 0;
 	}
-		
+
 }
